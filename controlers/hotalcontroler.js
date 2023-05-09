52466ae6794f1c4454e7dafdb4413a627db0b6ae
unknown <shaikimam916@gmail.com>
2023-05-09 21:10:26 +0530

    ....-

--- controlers/hotalcontroler.js
+++ controlers/hotalcontroler.js
@@ -1,9 +1,11 @@
 var hotals=require("../model/hotal.model");
 var hotal= async (req,res)=>{
-        const catagiry=req.query.catagiry;
+        const catagiry1=req.query.catagiry;
         try{
-            if(catagiry){
-           var hot=await hotals.find({ category:catagiry});
+            if(catagiry1){
+           var hot=await hotals.find({ category:catagiry1});
+            }else{
+                var hot=await hotals.find({});
             }
            hot? res.json(hot):res.status(404).json({message: "conud not fetch"});
         }
