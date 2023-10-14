914a0e384961b84f8d561eeddc758483144f4765
unknown <shaikimam916@gmail.com>
2023-10-15 01:05:59 +0530

    adding_proxy

--- controlers/proxy.js
+++ controlers/proxy.js
@@ -1,9 +1,14 @@
 var cors_proxy = require('cors-anywhere');
 var pr= async (req,res)=>{
+    var host = process.env.HOST || '127.0.0.1';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
     cors_proxy.createServer({
         originWhitelist: [], // Allow all origins
         requireHeader: ['origin', 'x-requested-with'],
         removeHeaders: ['cookie', 'cookie2']
+    }).listen(port, host, function() {
+        console.log('Running CORS Anywhere on ' + host + ':' + port);
     })
     res.json("new")
 }
